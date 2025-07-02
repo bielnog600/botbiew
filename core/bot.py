87de2d480f52fbbbc08312bf41f023599a75f83c
bielnog600 <bielnog600@gmail.com>
2025-07-03 00:54:53 +0200

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -73,7 +73,7 @@ class TradingBot:
         """
         now = datetime.now()
         # FIX: Margem de segurança aumentada para 5 segundos.
-        wait_time = (60 - now.second) + 10 if now.second > 10 else 10 - now.second
+        wait_time = (60 - now.second) + 15 if now.second > 15 else 15 - now.second
         await asyncio.sleep(wait_time)
 
     async def _process_asset_task(self, full_asset_name: str):
