e297a444cf9a9c4b90a757378642eedd9b3e8d39
bielnog600 <bielnog600@gmail.com>
2025-08-16 23:53:34 +0100

    Update exnova_service.py

--- services/exnova_service.py
+++ services/exnova_service.py
@@ -21,7 +21,7 @@ class ExnovaService:
         self.email = email
         self.password = password
         self.driver = None
-        # --- CORREÇÃO: Limpa sessões antigas ANTES de iniciar uma nova ---
+        # Limpa sessões antigas ANTES de iniciar uma nova
         self._cleanup_old_sessions()
         self._setup_driver()
 
